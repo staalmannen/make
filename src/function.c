8b0ebac1cab64af79266a2f572f529e2d1c5efe8
Jens Staal <staal1978@gmail.com>
2020-06-17 18:27:26 +0200

    I give up and just remove one argument.

--- src/function.c
+++ src/function.c
@@ -2391,9 +2391,8 @@ expand_builtin_function (char *o, int argc, char **argv,
                          const struct function_table_entry *entry_p)
 {
   if (argc < (int)entry_p->minimum_args)
-    OSN (fatal, *expanding_var,
-           _("insufficient number of arguments (%d) to function `%s'"),
-           argc, entry_p->name);
+    OS (fatal, *expanding_var,
+           _("insufficient number of arguments (%d) to function `%s'"), entry_p->name);
 
   /* I suppose technically some function could do something with no
      arguments, but so far none do, so just test it for all functions here
